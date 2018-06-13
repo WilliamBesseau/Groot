9dfdc05dc6da914a65a94a57b2b56dc63ddbdb8e
William BESSEAU <willibesse@hotmail.fr>
2018-06-13 16:23:10 +0200

    view & template dash

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import timedelta
 from pprint import pprint
 
 from flask import render_template, request, redirect, url_for, session, abort
@@ -5,6 +7,7 @@ from flask import render_template, request
 from sqlalchemy import or_
 
 from app import app, db
+from app.models import dash
 from app.models.dash import Dash
 
 @app.route('/dashboard')
@@ -13,14 +16,11 @@ def get_dashboard():
     Pagine et renvoie la liste des projets
     :return:
     """
-    last_dash = Dash.query.get(1)
-    print(last_dash)
-    print('**************************')
-    print('**************************')
-    print('**************************')
-    print('**************************')
-    print('**************************')
+    dash = Dash.query.get(2)
+    d = '1-1-1970 00:00:00' + timedelta(seconds=dash.air.date)
+
+    print(d)
     return render_template(
-        'dashboard.html'
-        # data=last_dash,
+        'dashboard.html',
+        dash=dash
     )
